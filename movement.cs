310ec2a46cd75315820bbb520da4a0400df49b64
Jaden D <45317852+GoldenJayz@users.noreply.github.com>
2021-02-13 08:09:23 -0500

    Add files via upload

---
+++ movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed = 100f;
+    public float jumpheight;
+    public bool inair = false;
+
+
+    private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        inair = false;
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        inair = true;
+    }
+   
+    void FixedUpdate()
+    {
+        Vector2 NoMovement = new Vector2(0f, 0f);
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        if (moveHorizontal > 0)
+        {
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        if (moveHorizontal < 0)
+        {
+            rb.velocity = new Vector2(-speed, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(0, jumpheight), ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ sensor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sensor : MonoBehaviour
+{
+    public float playerSpeed;
+    public Vector2 jumpHeight;
+
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Hit detected");
+        GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+    }
+}
